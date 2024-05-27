04219fdac2a5c1816cd08141513770adc0c2a48b
Daniil Shvalov <shvalovich@gmail.com>
2024-05-27 10:08:17 +0300

    fix!: change prefix from `git-commit` to `git-commit-ts`

--- git-commit-ts-mode.el
+++ git-commit-ts-mode.el
@@ -37,15 +37,15 @@
 (declare-function treesit-node-start "treesit.c")
 (declare-function treesit-node-child "treesit.c")
 
-(defgroup git-commit nil
+(defgroup git-commit-ts nil
   "Git commit commands."
   :group 'extensions)
 
-(defgroup git-commit-faces nil
+(defgroup git-commit-ts-faces nil
   "Faces used by git commit."
   :group 'git-commit)
 
-(defface git-commit-comment-face '((t :inherit font-lock-comment-face))
+(defface git-commit-ts-comment-face '((t :inherit font-lock-comment-face))
   "Face used for comments. Example:
 
    some commit message
@@ -53,10 +53,10 @@
    # this is my comment
    └──────────────────┘
 
-The underlined text will be highlighted using `git-commit-comment-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-comment-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-title-face '((t :inherit font-lock-string-face))
+(defface git-commit-ts-title-face '((t :inherit font-lock-string-face))
   "Face used for commit titles. Example:
 
    feat(some-module): some commit message
@@ -69,10 +69,10 @@ or:
    └───────────────┬────────────────┘
              commit title
 
-The underlined text will be highlighted using `git-commit-title-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-title-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-overflow-face '((t :inherit error))
+(defface git-commit-ts-overflow-face '((t :inherit error))
   "Face used for overflowed (> 50) commit titles. Example:
 
                      50 characters
@@ -81,10 +81,10 @@ The underlined text will be highlighted using `git-commit-title-face'."
                                                      └───────────┬──────────┘
                                                       overflowed commit title
 
-The underlined text will be highlighted using `git-commit-overflow-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-overflow-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-prefix-face '((t :inherit font-lock-function-name-face))
+(defface git-commit-ts-prefix-face '((t :inherit font-lock-function-name-face))
   "Face used for commit prefixes. Example:
 
    refactor(some-module): some commit message
@@ -97,10 +97,10 @@ or:
    └───┬───┘
        └── commit prefix
 
-The underlined text will be highlighted using `git-commit-prefix-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-prefix-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-type-face '((t :inherit font-lock-keyword-face))
+(defface git-commit-ts-type-face '((t :inherit font-lock-keyword-face))
   "Face used for commit prefix types. Example:
 
    refactor(some-module): some commit message
@@ -113,20 +113,20 @@ or:
    └──┬───┘
       └── commit prefix type
 
-The underlined text will be highlighted using `git-commit-type-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-type-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-scope-face '((t :inherit font-lock-variable-name-face))
+(defface git-commit-ts-scope-face '((t :inherit font-lock-variable-name-face))
   "Face used for commit prefix scopes. Example:
 
    refactor(some-module): some commit message
             └────┬────┘
         commit prefix scope
 
-The underlined text will be highlighted using `git-commit-scope-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-scope-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-punctuation-delimiter-face '((t :inherit font-lock-punctuation-face))
+(defface git-commit-ts-punctuation-delimiter-face '((t :inherit font-lock-punctuation-face))
   "Face used for common punctuation delimiters. Example:
 
    refactor(some-module): some commit message
@@ -134,10 +134,10 @@ The underlined text will be highlighted using `git-commit-scope-face'."
            └───────────┴┴─ common punctuation delimiters
 
 The underlined with caret text will be highlighted using
-`git-commit-punctuation-delimiter-face'."
-  :group 'git-commit-faces)
+`git-commit-ts-punctuation-delimiter-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-punctuation-special-face '((t :inherit font-lock-warning-face))
+(defface git-commit-ts-punctuation-special-face '((t :inherit font-lock-warning-face))
   "Face used for special punctuation delimiters. Example:
 
    refactor(some-module)!: some commit message
@@ -145,10 +145,10 @@ The underlined with caret text will be highlighted using
                         └── special punctuation delimiter
 
 The underlined with caret text will be highlighted using
-`git-commit-punctuation-special-face'."
-  :group 'git-commit-faces)
+`git-commit-ts-punctuation-special-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-token-face '((t :inherit font-lock-builtin-face))
+(defface git-commit-ts-token-face '((t :inherit font-lock-builtin-face))
   "Face used for tokens. Example:
 
    refactor(some-module): some commit message
@@ -157,10 +157,10 @@ The underlined with caret text will be highlighted using
    └──┬──┘
       └── token
 
-The underlined text will be highlighted using `git-commit-token-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-token-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-value-face '((t :inherit font-lock-variable-name-face))
+(defface git-commit-ts-value-face '((t :inherit font-lock-variable-name-face))
   "Face used for values. Example:
 
    refactor(some-module): some commit message
@@ -169,10 +169,10 @@ The underlined text will be highlighted using `git-commit-token-face'."
            └───┬───┘
              value
 
-The underlined text will be highlighted using `git-commit-value-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-value-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-breaking-change-token-face '((t :inherit font-lock-warning-face))
+(defface git-commit-ts-breaking-change-token-face '((t :inherit font-lock-warning-face))
   "Face used for breaking change tokens. Example:
 
    refactor(some-module): some commit message
@@ -182,10 +182,10 @@ The underlined text will be highlighted using `git-commit-value-face'."
           └── breaking change token
 
 The underlined text will be highlighted using
-`git-commit-breaking-change-token-face'."
-  :group 'git-commit-faces)
+`git-commit-ts-breaking-change-token-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-breaking-change-value-face '((t :inherit font-lock-variable-name-face))
+(defface git-commit-ts-breaking-change-value-face '((t :inherit font-lock-variable-name-face))
   "Face used for breaking change values. Example:
 
    refactor(some-module): some commit message
@@ -195,10 +195,10 @@ The underlined text will be highlighted using
                    breaking change value
 
 The underlined text will be highlighted using
-`git-commit-breaking-change-value-face'."
-  :group 'git-commit-faces)
+`git-commit-ts-breaking-change-value-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-filepath-face '((t :inherit font-lock-variable-name-face))
+(defface git-commit-ts-filepath-face '((t :inherit font-lock-variable-name-face))
   "Face used for filepath. Example:
 
    # Please enter the commit message for your changes. Lines starting
@@ -212,10 +212,10 @@ The underlined text will be highlighted using
                           filepath
    #
 
-The underlined text will be highlighted using `git-commit-filepath-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-filepath-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-change-face '((t :inherit font-lock-builtin-face))
+(defface git-commit-ts-change-face '((t :inherit font-lock-builtin-face))
   "Face used for changes. Example:
 
    # Please enter the commit message for your changes. Lines starting
@@ -228,10 +228,10 @@ The underlined text will be highlighted using `git-commit-filepath-face'."
            └── change
    #
 
-The underlined text will be highlighted using `git-commit-change-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-change-face'."
+  :group 'git-commit-ts-faces)
 
-(defface git-commit-branch-face '((t :inherit font-lock-keyword-face))
+(defface git-commit-ts-branch-face '((t :inherit font-lock-keyword-face))
   "Face used for branch names. Example:
 
    # Please enter the commit message for your changes. Lines starting
@@ -244,87 +244,87 @@ The underlined text will be highlighted using `git-commit-change-face'."
    #	renamed:    some-file -> other-file
    #
 
-The underlined text will be highlighted using `git-commit-branch-face'."
-  :group 'git-commit-faces)
+The underlined text will be highlighted using `git-commit-ts-branch-face'."
+  :group 'git-commit-ts-faces)
 
-(defvar git-commit--treesit-font-lock-settings
+(defvar git-commit-ts-font-lock-settings
   (treesit-font-lock-rules
    :feature 'comment
    :language 'gitcommit
-   '((comment) @git-commit-comment-face
-     (generated_comment) @git-commit-comment-face
-     (scissor) @git-commit-comment-face)
+   '((comment) @git-commit-ts-comment-face
+     (generated_comment) @git-commit-ts-comment-face
+     (scissor) @git-commit-ts-comment-face)
 
    :feature 'title
    :language 'gitcommit
-   '((subject) @git-commit-title-face)
+   '((subject) @git-commit-ts-title-face)
 
    :feature 'overflow
    :language 'gitcommit
    :override t
    '((subject
-      (overflow) @git-commit-overflow-face))
+      (overflow) @git-commit-ts-overflow-face))
 
    :feature 'prefix
    :language 'gitcommit
    :override t
    '((subject
-      (subject_prefix) @git-commit-prefix-face))
+      (subject_prefix) @git-commit-ts-prefix-face))
 
    :feature 'prefix
    :language 'gitcommit
    :override t
    '((prefix
-      (type) @git-commit-type-face)
+      (type) @git-commit-ts-type-face)
      (prefix
-      (scope) @git-commit-scope-face))
+      (scope) @git-commit-ts-scope-face))
 
    :feature 'punctuation
    :language 'gitcommit
    :override t
    '((prefix
-      ["(" ")" ":"] @git-commit-punctuation-delimiter-face)
+      ["(" ")" ":"] @git-commit-ts-punctuation-delimiter-face)
      (prefix
-      "!" @git-commit-punctuation-special-face)
-     (arrow) @git-commit-punctuation-delimiter-face)
+      "!" @git-commit-ts-punctuation-special-face)
+     (arrow) @git-commit-ts-punctuation-delimiter-face)
 
    :feature 'filepath
    :language 'gitcommit
    :override t
-   '((filepath) @git-commit-filepath-face)
+   '((filepath) @git-commit-ts-filepath-face)
 
    :feature 'branch
    :language 'gitcommit
    :override t
-   '((branch) @git-commit-branch-face)
+   '((branch) @git-commit-ts-branch-face)
 
    :feature 'change
    :language 'gitcommit
    :override t
-   '((change) @git-commit-change-face)
+   '((change) @git-commit-ts-change-face)
 
    :feature 'token
    :language 'gitcommit
    :override t
    '((trailer
-      (token) @git-commit-token-face)
+      (token) @git-commit-ts-token-face)
      (trailer
-      (value) @git-commit-value-face)
+      (value) @git-commit-ts-value-face)
      (breaking_change
-      (token) @git-commit-breaking-change-token-face)
+      (token) @git-commit-ts-breaking-change-token-face)
      (breaking_change
-      (value) @git-commit-breaking-change-value-face)
+      (value) @git-commit-ts-breaking-change-value-face)
      (generated_comment
-      (title) @git-commit-token-face)
+      (title) @git-commit-ts-token-face)
      (generated_comment
-      (value) @git-commit-value-face)))
+      (value) @git-commit-ts-value-face)))
   "Tree-sitter font-lock settings for `git-commit-ts-mode'.")
 
 ;;;###autoload
 (define-derived-mode git-commit-ts-mode prog-mode "Git commit"
   (when (treesit-ready-p 'gitcommit)
     (treesit-parser-create 'gitcommit)
-    (setq-local treesit-font-lock-settings git-commit--treesit-font-lock-settings)
+    (setq-local treesit-font-lock-settings git-commit-ts-font-lock-settings)
     (setq-local treesit-font-lock-feature-list
                 '((comment title)
                   (token overflow)
