2ad5ca0fe52f165d3bc885b82dac68f093557778
Daniil Shvalov <shvalovich@gmail.com>
2024-05-27 10:02:37 +0300

    fix: add licence boilerplate

--- git-commit-ts-mode.el
+++ git-commit-ts-mode.el
@@ -5,9 +5,24 @@
 ;; Author: Daniil Shvalov <daniil.shvalov@gmail.com>
 ;; Version: 1.0
 ;; Package-Requires: ((emacs "29.1"))
-;; License: GPL-3.0-or-later
+;; SPDX-License-Identifier: GPL-3.0-or-later
 ;; Keywords: tree-sitter, git, faces
 ;; Homepage: https://github.com/danilshvalov/git-commit-ts-mode
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
